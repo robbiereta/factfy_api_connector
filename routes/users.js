de0dcff691f132dec8b03106de32e9ce37950078
robbiereta <robbie.reta67@gmail.com>
2021-09-28 12:45:35 -0500

    initial commit

--- routes/users.js
+++ routes/users.js
@@ -1,70 +1,44 @@
 var express = require("express");
 var router = express.Router();
-
+var token;
 /* GET users listing. */
 router.get("/", function (req, res, next) {
+  res.send(token);
   var axios = require("axios");
-  var resp;
   var data = JSON.stringify({
-    emisor: {
-      uuid: "507d0fa0-9496-424d-9b43-a01a25843f98"
-    },
-    receptor: {
-      uuid: "c9d2ba34-53f9-45cd-83ff-f29ebc3e39e2"
-    },
-    factura: {
-      fecha: "2019-07-25 10:22:18",
-      tipo: "ingreso",
-      generacion_automatica: true,
-      subtotal: 2001,
-      impuesto_federal: 320,
-      total: 2320,
-      conceptos: [
-        {
-          clave_producto_servicio: "76111500",
-          clave_unidad_de_medida: "E48",
-          cantidad: 1,
-          descripcion: "SERVICIO DE LIMPIEZA",
-          valor_unitario: 2001,
-          total: 2001
-        }
-      ]
-    }
+    api_key: "KmXZZCWXXNTAHJrCR3CPYjPNil5jSynsRnlbaOZOgwF8CETPtAAIRdp9LGQb",
+    api_secret: "aEyfXpHwBYPOUfyBgDYLWfNmtALpl0Kh"
   });
 
   var config = {
     method: "post",
-    url: "https://api.facturify.com/api/v1/factura",
+    url: "https://api.facturify.com/api/v1/auth",
     headers: {
-      Authorization:
-        "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJodHRwczpcL1wvYXBpLmZhY3R1cmlmeS5jb21cL2FwaVwvdjFcL2F1dGgiLCJpYXQiOjE2MzI2OTUwMDUsImV4cCI6MTYzMjc4MTQwNSwibmJmIjoxNjMyNjk1MDA1LCJqdGkiOiJjRkRKRzF2aVl2d0ExTkNRIiwic3ViIjo4MzM0NCwicHJ2IjoiMGE1YjkwMDBkMzRhMTM5NjExOGU1NDgzNDJlYzQ0MDE2ZjA4YzMzMSJ9.jooxNPOshcisYmrgdBSf-I5b2CeJG-7a3XNWJNeGikYnkjzp-0u_ew84HutYgt9tD8LxPl2gOAwjN4Kn_Bc9t",
-      "Content-Type": "application/json"
+      "Content-Type": "application/json",
+      "cache-control": "no-cache"
     },
     data: data
   };
 
   axios(config)
     .then(function (response) {
-      console.log(JSON.stringify(response.data));
-      resp = response;
+      token = response.data.jwt.token;
     })
     .catch(function (error) {
       console.log(error);
     });
-  res.send("solicitud exitosa");
 });
 
 router.post("/", function (req, res, next) {
   var axios = require("axios");
   var data = req.body;
-  console.log(req.body);
+  // console.log(req.body.token);
 
   var config = {
     method: "post",
     url: "https://api.facturify.com/api/v1/factura",
     headers: {
-      Authorization:
-        "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJodHRwczpcL1wvYXBpLmZhY3R1cmlmeS5jb21cL2FwaVwvdjFcL2F1dGgiLCJpYXQiOjE2MzI2OTUwMDUsImV4cCI6MTYzMjc4MTQwNSwibmJmIjoxNjMyNjk1MDA1LCJqdGkiOiJjRkRKRzF2aVl2d0ExTkNRIiwic3ViIjo4MzM0NCwicHJ2IjoiMGE1YjkwMDBkMzRhMTM5NjExOGU1NDgzNDJlYzQ0MDE2ZjA4YzMzMSJ9.jooxNPOshcisYmrgdBSf-I5b2CeJG-7a3XNWJNeGikYnkjzp-0u_ew84HutYgt9tD8LxPl2gOAwjN4Kn_Bc9t",
+      Authorization: "Bearer " + token,
       "Content-Type": "application/json"
     },
     data: data
@@ -73,11 +47,13 @@ router.post("/", function (req, res, next) {
   axios(config)
     .then(function (response) {
       console.log(JSON.stringify(response.data));
+      res.send("solicitud exitosa post");
     })
     .catch(function (error) {
       console.log(error);
     });
-  res.send("solicitud exitosa post");
 });
+
+router.post("/token", function (req, res, next) {});
 
 module.exports = router;
